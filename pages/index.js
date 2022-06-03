cbd58593a0d00ea3e6db11869c6bc9f4f19c6390
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2022-06-03 21:59:34 +0000

    Added State and content

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
 const Index = () => {
-	return <></>;
+	var [gateState, setGateState] = useState(false);
+	return (
+		<>
+			<span>
+				<h1>{gateState ? "yes" : "no"}</h1>
+				<button onClick={() => setGateState(!gateState)}>
+					Toggle gateState
+				</button>
+			</span>
+		</>
+	);
 };
 
 export default Index;
