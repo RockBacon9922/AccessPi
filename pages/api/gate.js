ce8400d6a0ccbd721bd2aee4f401cf08b7e219fe
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2022-06-04 01:00:35 +0000

    Gate Status now persists. txt file!

--- pages/api/gate.js
+++ pages/api/gate.js
@@ -1,20 +1,26 @@
 import { Gpio } from "onoff";
 import { useState } from "react";
+import fs from "fs";
 // const gate = new Gpio(4, "out");
+//open file called gate status
 export default function handler(req, res) {
-	console.log(req.query);
+	var status = fs.readFileSync("gateStatus", "utf8");
 	// console.log(req.query);
-	var status = false;
-	// gate.writeSync(status ? 1 : 0);
-	// check if key value gate is close
 	if (req.query.gate === "close") {
 		// gate.writeSync(0);
+		//write false to file
+		fs.writeFileSync("gateStatus", "false");
 		status = false;
-		res.status(200).json({ message: "Gate is closed", status: status });
+		res
+			.status(200)
+			.json({ message: "Success: Gate is closing", status: status });
 	} else if (req.query.gate === "open") {
+		fs.writeFileSync("gateStatus", "true");
 		status = true;
 		// gate.writeSync(1);
-		res.status(200).json({ message: "Gate is open", status: status });
+		res
+			.status(200)
+			.json({ message: "Success: Gate is opening", status: status });
 	} else {
 		res.status(207).json({
 			message: "No Command has been sent to the gate",
