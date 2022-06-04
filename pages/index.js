ada2c8645ac016976eebd9ca18a6aab49ccba92c
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2022-06-04 00:18:49 +0000

    Gate Frontend talks with backend

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useReducer } from "react";
+import axios from "axios";
+
+const fetchStatus = () => {
+	fetch("/api/gate")
+		.then((res) => res.json())
+		.then((data) => {
+			return data.status;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+var apiCallState = false;
+
 const Index = () => {
-	var [gateState, setGateState] = useState(false);
+	const [gateState, setGateState] = useState(false);
+	var apiCall = (state) => {
+		if (apiCallState) {
+			setGateState(!gateState);
+			axios({
+				method: "post",
+				url: "/api/gate",
+				params: {
+					gate: state ? "open" : "close",
+				},
+			}).then((res) => {
+				console.log(res);
+			});
+		} else {
+			apiCallState = !apiCallState;
+		}
+	};
 	return (
 		<>
 			<span>
 				<h1>{gateState ? "yes" : "no"}</h1>
-				<button onClick={() => setGateState(!gateState)}>
-					Toggle gateState
-				</button>
+				<button onClick={() => apiCall(!gateState)}>Toggle gateState</button>
 			</span>
 		</>
 	);
